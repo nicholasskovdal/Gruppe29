3600d7ddb4fd81f8846f3e5bc8506b5ac8892679
Nicholas <nbsk67234@edu.ucl.dk>
2025-09-15 21:58:27 +0200

    IProductRepository added

---
+++ UCLProjekter/Dive Deep/Persistence/IProductRepository.cs
@@ -0,0 +1,22 @@
+﻿using Dive_Deep.Models;
+namespace Dive_Deep.Persistence
+{
+    public interface IProductRepository
+    {
+
+        //Add. Asynchronous; the Task keyword represents an on-going operation that might take a while to finish. If there's no return type specified in Task<T>, it's like Void
+        Task AddAsync(Product product);
+
+        //Read
+        Task<IEnumerable<Product>> GetAllAsync(); 
+        Task<Product?> GetByIdAsync(int productId);
+        Task<IEnumerable<T>> GetAllOfTypeAsync<T>() where T : Product; //Asynchronous GetAll of specified class that inherits from Product
+
+        //Update
+        Task UpdateAsync(Product product);
+
+        //Delete
+        Task DeleteAsync(int productId);
+
+    }
+}
--- UCLProjekter/Dive Deep/Persistence/ProductRepository.cs
+++ UCLProjekter/Dive Deep/Persistence/ProductRepository.cs
@@ -3,92 +3,36 @@ using System.Threading.Tasks;
 
 namespace Dive_Deep.Persistence
 {
-    public static class ProductRepository
+    public class ProductRepository : IProductRepository
     {
-        private static List<Product> _products = new List<Product>()
+        public Task AddAsync(Product product)
         {
-        };
-
-        //Create
-        public static void Add(Product product)
-        {
-            if (product == null) return;
-            product.ProductId = _products.Any() ? _products.Max(x => x.ProductId) + 1 : 1;
-            _products.Add(product);
+            throw new NotImplementedException();
         }
 
-        //Read
-        public static List<Product> GetAll()
+        public Task DeleteAsync(int productId)
         {
-            return _products;
+            throw new NotImplementedException();
         }
 
-        //Read
-        public static Product? GetById(int id)
+        public Task<IEnumerable<Product>> GetAllAsync()
         {
-            return _products.FirstOrDefault(x => x.ProductId == id);
+            throw new NotImplementedException();
         }
 
-        //Update
-        public static void Update(int productId, Product product) //Denne metode kræver, at det Product-object man passer som argument til parameteret allerede har fået sat alle sine values før metoden udføres
+        public Task<IEnumerable<T>> GetAllOfTypeAsync<T>() where T : Product
         {
-            var productToUpdate = GetById(productId);
-
-            productToUpdate.Brand = product.Brand; //disse to properties kommer fra parent class, som den kan jeg tage fat i med det samme uden at type checke og caste
-            productToUpdate.PricePerDay = product.PricePerDay;
-
-            if (productToUpdate != null)
-            {
-                if (productToUpdate is BCD bcd && product is BCD updatedBcd)
-                {
-                    bcd.Model = updatedBcd.Model;
-                    bcd.Sizes = updatedBcd.Sizes;
-                }
-                else if (productToUpdate is DivingSuit divingSuit && product is DivingSuit updatedDivingSuit)
-                {
-                    divingSuit.Model = updatedDivingSuit.Model;
-                    divingSuit.Sizes = updatedDivingSuit.Sizes;
-                    divingSuit.Type = updatedDivingSuit.Type;
-                    divingSuit.Gender = updatedDivingSuit.Gender;
-                    divingSuit.Thickness = updatedDivingSuit.Thickness;
-                }
-                else if (productToUpdate is Finns finns && product is Finns updatedFinns)
-                {
-                    finns.Model = updatedFinns.Model;
-                    finns.Sizes = updatedFinns.Sizes;
-                }
-                else if (productToUpdate is MaskSnorkel maskSnorkel && product is MaskSnorkel updatedMaskSnorkel)
-                {
-                    maskSnorkel.Model = updatedMaskSnorkel.Model;
-                }
-                else if (productToUpdate is RegulatorSet regulatorSet && product is RegulatorSet updatedRegulatorSet)
-                {
-                    regulatorSet.FirstStep = updatedRegulatorSet.FirstStep;
-                    regulatorSet.SecondStep = updatedRegulatorSet.SecondStep;
-                    regulatorSet.Octopus = updatedRegulatorSet.Octopus;
-                }
-                else if (productToUpdate is Tank tank && product is Tank updatedTank)
-                {
-                    tank.Volume = updatedTank.Volume;
-                }
-
-            }
+            throw new NotImplementedException();
         }
 
-        static ProductRepository()
+        public Task<Product?> GetByIdAsync(int productId)
         {
-            Add(new BCD { Brand = "Scubapro", Model = "Navigator Lite BCD", PricePerDay = 125 });
-            Add(new BCD { Brand = "Scubapro", Model = "BCD Glide", PricePerDay = 140 });
-            Add(new BCD { Brand = "Scubapro", Model = "BCD Hydros Pro", PricePerDay = 200 });
-            Add(new BCD { Brand = "Seac", Model = "BCD Modular", PricePerDay = 145 });
-            Add(new DivingSuit { Brand = "Scubapro", Model = "Definition", Sizes = "XS, S, M, L, XL", Type = "Våddragt", Gender = "Herre/Dame", Thickness = 3f, PricePerDay = 100 });
-            Add(new DivingSuit { Brand = "Scubapro", Model = "Definition", Sizes = "XS, S, M, L, XL", Type = "Våddragt", Gender = "Herre/Dame", Thickness = 5f, PricePerDay = 100 });
-            Add(new DivingSuit { Brand = "Scubapro", Model = "Definition", Sizes = "XS, S, M, L, XL", Type = "Våddragt", Gender = "Herre/Dame", Thickness = 7f, PricePerDay = 100 });
-            Add(new DivingSuit { Brand = "Waterproof", Model = "W5", Sizes = "XS, S, M, L, XL", Type = "Våddragt", Gender = "Herre/Dame", Thickness = 3.5f, PricePerDay = 100 });
-            Add(new DivingSuit { Brand = "Fourth Element", Model = "Proteus", Sizes = "XS, S, M, L, XL", Type = "Våddragt", Gender = "Herre/Dame", Thickness = 5f, PricePerDay = 120 });
-            Add(new DivingSuit { Brand = "Scubapro", Model = "Exodry 4.0", Sizes = "XS, S, M, L, XL", Type = "Tørdragt", Gender = "Herre/Dame", Thickness = null, PricePerDay = 300 });
-            Add(new DivingSuit { Brand = "Waterproof", Model = "D7 Evo", Sizes = "XS, S, M, L, XL", Type = "Tørdragt", Gender = "Herre/Dame", Thickness = null, PricePerDay = 320 });
-            Add(new DivingSuit { Brand = "Santi", Model = "E.Lite Plus", Sizes = "XS, S, M, L, XL", Type = "Tørdragt", Gender = "Herre/Dame", Thickness = null, PricePerDay = 350 });
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Product product)
+        {
+            throw new NotImplementedException();
         }
     }
 }
