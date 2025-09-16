06b89cb43440261241691feec17deb7e7c8619b0
Nicholas <nbsk67234@edu.ucl.dk>
2025-09-16 13:04:33 +0200

    bad code for ProductsController

--- UCLProjekter/Dive Deep/Controllers/ProductsController.cs
+++ UCLProjekter/Dive Deep/Controllers/ProductsController.cs
@@ -1,40 +1,55 @@
-﻿using Dive_Deep.Models;
+﻿using Dive_Deep.Data;
+using Dive_Deep.Models;
 using Dive_Deep.Persistence;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 
 namespace Dive_Deep.Controllers
 {
     public class ProductsController : Controller
     {
+        private readonly DiveDeepContext _context;
+        public ProductsController(DiveDeepContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index(string category)
         {
-            var allProducts = ProductRepository.GetAll();
-            IEnumerable<Product> products = allProducts;
-
-            string title = "Products";                          //For displaying @Render title nicely;
+            IQueryable<Product> products = _context.Products;
 
             if (!string.IsNullOrEmpty(category))
             {
-                products = category.ToLower() switch
+                switch (category.ToLower())
                 {
-                    "bcd" => allProducts.OfType<BCD>(),
-                    "divingsuit" => allProducts.OfType<DivingSuit>(),
-                    "tank" => allProducts.OfType<Tank>(),
-                    "regulatorset" => products.OfType<RegulatorSet>(),
-                    "masksnorkel" => products.OfType<MaskSnorkel>(),
-                    "finns" => allProducts.OfType<Finns>()
-                };
-
-                title = category.ToLower() switch               //Sætter title (string) til en kategori ud fra category input parameter som kommer fra knap i drowdown>_Layout
-                {
-                    "bcd" => "BCD",
-                    "divingsuit" => "Dykkerdragter",
-                    "tank" => "Tanke",
-                };
+                    case "bcd":
+                        products = _context.BCDs;
+                        break;
+                    case "divingsuit":
+                        products = _context.BCDs;
+                        break;
+                    case "tank":
+                        products = _context.Tanks;
+                        break;
+                    case "fins":
+                        products = _context.Finns;
+                        break;
+                    case "mask":
+                        products = _context.MaskSnorkels;
+                        break;
+                    case "regulator":
+                        products = _context.RegulatorSets;
+                        break;
+                }
             }
-            ViewData["Title"] = title;
-            return View(products);
+
+            // Gruppér på Brand+Model så du kun får én per kort
+            var grouped = await products
+                .GroupBy(p => new { p.Brand, p.Model })
+                .Select(g => g.First())  // tager bare en repræsentant
+                .ToListAsync();
+
+            return View(grouped);
         }
     }
 }
--- UCLProjekter/Dive Deep/Persistence/ProductRepository.cs
+++ UCLProjekter/Dive Deep/Persistence/ProductRepository.cs
@@ -13,14 +13,14 @@ namespace Dive_Deep.Persistence
             _context = context;
         }
 
-        //Create
+        
         public async Task AddAsync(Product product)
         {
             _context.Products.Add(product);
             await _context.SaveChangesAsync();
         }
 
-        //Reads
+        
         public async Task<IEnumerable<Product>> GetAllAsync()
         {
             return await _context.Products.ToListAsync();
@@ -37,7 +37,7 @@ namespace Dive_Deep.Persistence
         }
 
 
-        //Update
+        
         public async Task UpdateAsync(Product product)
         {
             _context.Products.Update(product);
@@ -45,7 +45,7 @@ namespace Dive_Deep.Persistence
         }
 
 
-        //Delete
+        
         public async Task DeleteAsync(int productId)
         {
             var product = await GetByIdAsync(productId);
