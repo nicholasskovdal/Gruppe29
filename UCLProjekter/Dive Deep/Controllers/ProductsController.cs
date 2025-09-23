2639e76fbba8c0d1d4841a00a283e8f0b290d905
Nicholas <nbsk67234@edu.ucl.dk>
2025-09-23 18:54:11 +0200

    ProductRepository method for grouping product categories together and Products>Index view shows all products on their own page

--- UCLProjekter/Dive Deep/Controllers/ProductsController.cs
+++ UCLProjekter/Dive Deep/Controllers/ProductsController.cs
@@ -10,47 +10,20 @@ namespace Dive_Deep.Controllers
 {
     public class ProductsController : Controller
     {
-        private readonly DiveDeepContext _context;
-        public ProductsController(DiveDeepContext context)
+        private readonly IProductRepository _productRepo;
+        public ProductsController(IProductRepository productRepo)
         {
-            _context = context;
+            _productRepo = productRepo;
         }
+
+
+
         public async Task<IActionResult> Index(string category)
         {
-            IQueryable<Product> products = _context.Products;
-
-            if (!string.IsNullOrEmpty(category))
-            {
-                switch (category.ToLower())
-                {
-                    case "bcd":
-                        products = _context.Products.OfType<BCD>();
-                        break;
-                    case "divingsuit":
-                        products = _context.Products.OfType<DivingSuit>();
-                        break;
-                    case "tank":
-                        products = _context.Products.OfType<Tank>();
-                        break;
-                    case "fins":
-                        products = _context.Products.OfType<Finns>();
-                        break;
-                    case "mask":
-                        products = _context.Products.OfType<MaskSnorkel>();
-                        break;
-                    case "regulator":
-                        products = _context.Products.OfType<RegulatorSet>();
-                        break;
-                }
-            }
-
-            // Gruppér på Brand+Model så du kun får én per kort
-            var grouped = await products
-                .GroupBy(p => new { p.Brand})
-                .Select(g => g.First())  // tager bare en repræsentant
-                .ToListAsync();
-
-            return View(grouped);
+            var products = await _productRepo.GetGroupedProductsByCategory(category);
+            return View(products);
         }
+
+
     }
 }
