7f44fc420b380e1bdd269f48628b505842edc198
Nicholas <nbsk67234@edu.ucl.dk>
2025-09-26 14:54:57 +0200

    Product Details view er oppe og køre og viser rigtige properties og info osv.

--- UCLProjekter/Dive Deep/Controllers/ProductsController.cs
+++ UCLProjekter/Dive Deep/Controllers/ProductsController.cs
@@ -5,6 +5,7 @@ using Dive_Deep.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Runtime.Intrinsics.X86;
 
 
 namespace Dive_Deep.Controllers
@@ -32,13 +33,48 @@ namespace Dive_Deep.Controllers
             if (product == null) return NotFound();
             
             var variants = await _productRepo.GetAllProductsInSameGroup(product);
-
-
+           
             var vm = new ProductDetailsViewModel
             {
                 Representative = product,
                 Variants = variants.ToList()
             };
+
+            switch (product)
+            {
+                case BCD bcd:
+                    vm.RepresentativeSecondaryLabel = "Model";
+                    vm.RepresentativeSecondaryValue = bcd.Model;
+                    break;
+                case DivingSuit divingSuit:
+                    vm.RepresentativeSecondaryLabel = "Model";
+                    vm.RepresentativeSecondaryValue = divingSuit.Model;
+                    var suits = vm.Variants.OfType<DivingSuit>();
+                    vm.Sizes = suits.Select(s => s.Sizes).Distinct().ToList();
+                    vm.Genders = suits.Select(s => s.Gender).Distinct().ToList();
+                    vm.Thicknesses = suits.Where(s => s.Thickness != null).Select(s => s.Thickness!).Distinct().ToList();
+                    break;
+                case Tank tank:
+                    vm.RepresentativeSecondaryLabel = "Volumen";
+                    vm.RepresentativeSecondaryValue = tank.Volume.ToString() + " liter";
+                    break;
+                case RegulatorSet regulatorSet:
+                    vm.RepresentativeSecondaryLabel = "1. Trin";
+                    vm.RepresentativeSecondaryValue = regulatorSet.FirstStep.ToString();
+                    break;
+                case MaskSnorkel maskSnorkel:
+                    vm.RepresentativeSecondaryLabel = "Model";
+                    vm.RepresentativeSecondaryValue = maskSnorkel.Model;
+                    break;
+                case Finns finns:
+                    vm.RepresentativeSecondaryLabel = "Model";
+                    vm.RepresentativeSecondaryValue = finns.Model;
+                    break;
+                default:
+                    vm.RepresentativeSecondaryLabel = string.Empty;
+                    vm.RepresentativeSecondaryValue = string.Empty;
+                    break;
+            }
             return View(vm);
         }
     }
--- UCLProjekter/Dive Deep/ViewModels/ProductDetailsViewModel.cs
+++ UCLProjekter/Dive Deep/ViewModels/ProductDetailsViewModel.cs
@@ -5,6 +5,22 @@ namespace Dive_Deep.ViewModels
     public class ProductDetailsViewModel
     {
         public Product Representative { get; set; }
-        public List<Product> Variants { get; set; } = new List<Product>();
+        public List<Product> Variants { get; set; } = new();
+        public int SelectedProductId { get; set; }
+        
+
+
+        public List<string> Sizes { get; set; } = new();
+        public List<string> Genders { get; set; } = new();
+        public List<float?> Thicknesses { get; set; } = new();
+
+
+        public string SelectedSize { get; set; }
+        public string SelectedGender { get; set; }
+        public string SelectedThickness { get; set; }
+
+        public string RepresentativeSecondaryLabel { get; set; } = string.Empty;
+        public string RepresentativeSecondaryValue { get; set; } = string.Empty;
+
     }
 }
